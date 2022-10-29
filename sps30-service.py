581d50296832ae1d5c6ef1dc13634909f4176574
codyc1515 <Cody.Cooper@codenui.co.nz>
2022-10-29 19:26:42 +1300

    Update sps30-service.py

--- sps30-service.py
+++ sps30-service.py
@@ -1,1 +1,3 @@
 # Find it at https://github.com/UnravelTEC/Raspi-Driver-SPS30/blob/master/sps30-service.py
+# Then replace the LOGFILE variable with /tmp/sps30.log
+# Ignore the errors about mkdir or comment it out
